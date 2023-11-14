28141cdfcd7e3a46f7479c852324dba7f70ba0a9
Cenk Yenikoylu <mcy@dotnetmcy.net>
2023-11-14 13:44:02 +0300

    game.cs

--- GameStore.Client/Models/Game.cs
+++ GameStore.Client/Models/Game.cs
@@ -2,5 +2,9 @@ namespace GameStore.Client.Models;
 
 public class Game
 {
-
+    public int Id { get; set; }
+    public required string Name { get; set; } = String.Empty;
+    public required string Genre { get; set; }
+    public decimal Price { get; set; }
+    public DateTime ReleaseDate { get; set; }
 }
